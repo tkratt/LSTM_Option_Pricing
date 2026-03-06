c9093a1903dde4f95f8551430c1306685040d311
zhangluolinyouxiang-collab <zhangluolinyouxiang@gmail.com>
2026-03-06 06:50:29 -0500

    Update Run_model_with_all_features.py

--- LSTM Modeling/Run_model_with_all_features.py
+++ LSTM Modeling/Run_model_with_all_features.py
@@ -15,7 +15,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
 """
 Modifications:
-Spliy after scaling
+Split before scaling
 Fill NaN macro data with previous available data, NaN return data with 0
 Annualized vol% calculated without multiplying by 100
 Softplus for output layer
